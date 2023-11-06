d981fd9374e05ee4c7cf5aed529f5fe630791d86
Juan David <juandavid918@gmail.com>
2023-11-06 12:53:13 -0500

    Generando logs con Winston

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,16 +1,16 @@
-const { getAge, getUUID } = require("./plugins");
-const { http } = require("./plugins");
+const { buildLogger } = require("./plugins");
 
 // const { emailTemplate } = require("./js-foundation/01-template");
 // require("./js-foundation/02-destructuring");
 // const { getUserById } = require("./js-foundation/03-callbacks");
 // const { getUserById } = require("./js-foundation/04-arrow");
 // const { buildMakePerson } = require("./js-foundation/05-factory");
-const getPokemonById = require("./js-foundation/06-promises");
+// const getPokemonById = require("./js-foundation/06-promises");
 
-getPokemonById(1)
-    .then((pokemon) => console.log({ pokemon }))
-    .catch((err) => console.log({ err }));
+const logger = buildLogger("app.js");
+
+logger.log("Hola mundo");
+logger.error("Esto es algo malo");
 
 
 // ! Referencia a la función factory y uso
