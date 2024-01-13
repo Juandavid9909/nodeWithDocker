5ec657f71b99b506d00d5d61e8d66be125c23353
Juan David <juandavid918@gmail.com>
2024-01-13 16:57:55 -0500

    Correos electrónicos implementados en NOC

--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -3,27 +3,29 @@ import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
+import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 
 const fileSystemLogRepository = new LogRepositoryImpl(
     new FileSystemDatasource()
 );
+
+const emailService = new EmailService();
 
 export class Server {
 
     public static start() {
         console.log("Server started...");
 
-        const emailService = new EmailService();
+        // new SendEmailLogs(
+        //     emailService,
+        //     fileSystemLogRepository
+        // ).execute(
+        //     ["juandavid@mail.com", "juandavid2@mail.com"]
+        // );
 
-        emailService.sendEmail({
-            to: "jvarela@xrm.com.co",
-            subject: "Logs de sistema",
-            htmlBody: `
-                <h3>Logs de sistema - NOC</h3>
-                <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.</p>
-                <p>Ver logs adjuntos</p>
-            `
-        });
+        // emailService.sendEmailFileWithFileSystemLogs(
+        //     ["juandavid@mail.com", "juandavid2@mail.com"]
+        // );
 
         // CronService.createJob(
         //     "*/5 * * * * *",
