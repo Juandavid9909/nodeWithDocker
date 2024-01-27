b7298fa757e6f3495efef67eb019a51b108642f8
Juan David <juandavid918@gmail.com>
2024-01-27 15:58:04 -0500

    Proyecto finalizado

--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -3,19 +3,31 @@ import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
+import { LogSeverityLevel } from "../domain/entities/log.entity";
 import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
+import { PostgresLogDatasource } from "../infrastructure/datasources/postgres-log.datasource";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
+import { CheckServiceMultiple } from "../domain/use-cases/checks/check-service-multiple";
 
-const logRepository = new LogRepositoryImpl(
-    // new FileSystemDatasource(),
-    new MongoLogDatasource(),
+const fsLogRepository = new LogRepositoryImpl(
+    new FileSystemDatasource(),
+    // new MongoLogDatasource(),
+    // new PostgresLogDatasource(),
+);
+
+const mongoLogRepository = new LogRepositoryImpl(
+    new MongoLogDatasource()
+);
+
+const postgresLogRepository = new LogRepositoryImpl(
+    new PostgresLogDatasource()
 );
 
 const emailService = new EmailService();
 
 export class Server {
 
-    public static start() {
+    public static async start() {
         console.log("Server started...");
 
         // new SendEmailLogs(
@@ -24,6 +36,9 @@ export class Server {
         // ).execute(
         //     ["juandavid@mail.com", "juandavid2@mail.com"]
         // );
+
+        // const logs = await logRepository.getLogs(LogSeverityLevel.low);
+        // console.log(logs);
 
         // emailService.sendEmailFileWithFileSystemLogs(
         //     ["juandavid@mail.com", "juandavid2@mail.com"]
@@ -34,8 +49,8 @@ export class Server {
             () => {
                 const url = "https://google.com";
 
-                new CheckService(
-                    logRepository,
+                new CheckServiceMultiple(
+                    [fsLogRepository, mongoLogRepository, postgresLogRepository],
                     () => console.log(`${ url } is ok`),
                     (error) => console.log(error)
                 ).execute(url);
