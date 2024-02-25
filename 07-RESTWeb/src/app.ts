5e15c53c98e4571baf2d839aff253f5f10f1b5ce
Juan David <juandavid918@gmail.com>
2024-02-25 12:17:51 -0500

    Rest server con base de datos local funcionando

--- 07-RESTWeb/src/app.ts
+++ 07-RESTWeb/src/app.ts
@@ -1,3 +1,4 @@
+import { AppRoutes } from "./presentation/routes";
 import { envs } from "./config/envs";
 import { Server } from "./presentation/server";
 
@@ -8,6 +9,7 @@ import { Server } from "./presentation/server";
 function main() {
     const server = new Server({
         port: envs.PORT,
+        routes: AppRoutes.routes,
         public_path: envs.PUBLIC_PATH
     });
 
