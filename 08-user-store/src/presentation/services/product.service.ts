eb46403fea06555562f11671ab2909d09e6deae4
Juan David <juandavid918@gmail.com>
2025-02-01 09:23:48 -0500

    Relaciones y semillas completadas

--- 08-user-store/src/presentation/services/product.service.ts
+++ 08-user-store/src/presentation/services/product.service.ts
@@ -32,6 +32,8 @@ export class ProductService {
                 ProductModel.find()
                     .skip((page - 1) * limit)
                     .limit(limit)
+                    .populate("user")
+                    .populate("category")
             ]);
 
             return {
